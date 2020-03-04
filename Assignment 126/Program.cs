8a6fecd948317b3cf0aa536b7a56b5d66bcae385
Richard <40587587+Quetives@users.noreply.github.com>
2020-03-03 20:21:39 -0800

    Add project files.

---
+++ Assignment 126/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number between 1 and 10?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isguessed = number == 9;
+
+            do
+            {
+                switch (number)
+                {
+                    case 9:
+                        Console.WriteLine("You guessed the number 9. That is correct!");
+                        isguessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("You are wrong!");
+                        Console.WriteLine("Guess again?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isguessed);
+            Console.Read();
+
+            // do while above. while below.
+
+            int x = 0;
+            while(x < 10)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
